540743fa8a8f747ff6d1245a92d7f6f3fd7f7eea
saidabouabidi-ux <saida.bouabidi@gmail.com>
2026-03-01 15:17:42 -0500

    Create calculatrice.py

---
+++ src/calculatrice.py
@@ -0,0 +1,13 @@
+def addition(a, b):
+    return a + b
+
+def soustraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Division par zéro interdite")
+    return a / b
